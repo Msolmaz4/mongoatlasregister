9a0c70312e6e985fbf6b7709ca0f93f3b1661eba
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-09-02 21:58:37 +0200

    navbar

---
+++ profil/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar
+    <img src="2.jpg" alt="" />
+ <a href='#'>
+ <ul>
+            <li>Home </li>
+            <li> About </li>
+            <li> Services</li>
+            <li> Portfoli</li>
+            <li>Blog</li>
+           <li>Contact</li> 
+        </ul>
+ </a>
+       
+    </div>
+  )
+}
+
+export default Navbar
