2142e738768c276d88dd3851e9f8ac7113bde015
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-09-13 21:33:30 +0200

    search

---
+++ searchBasit/src/App.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import './App.css';
+
+function App() {
+
+  const [ load,setLoad] = useState([])
+  const [input,setInput] =useState('')
+  const [filter,setFilter] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>setLoad(res.data))
+  },[])
+
+  useEffect(()=>{
+    setFilter(
+      load.filter((er)=>er.name.toLowerCase().includes(input.toLowerCase()))
+    )
+
+
+
+  },[load,input])
+ 
+  console.log(input)
+
+  return (
+    <div className="App">
+
+      <input type='search'  onChange={(e)=>setInput(e.target.value)}/>
+      {
+        filter.map((el)=>(
+          <div>
+            {el.name}
+          </div>
+        ))
+      }
+
+
+      
+      
+
+
+  
+    </div>
+  );
+}
+
+export default App;
