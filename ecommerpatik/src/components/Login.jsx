da43af6bf59b1ae67773ffe269e3efde5b0cbc65
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-09-22 19:47:07 +0200

    motion

--- ecommerpatik/src/components/Login.jsx
+++ ecommerpatik/src/components/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='log'>
+      <div className='recht'>
+     
+      <motion.div animate={{ 
+        rotate:[0,200,200,0],
+        x:[0,200,200,0,-200,-200,0]
+      }}
+       transition={{repeat:Infinity,duration:1}}
+      >
+      <div className='der'></div>
+      </motion.div>
+      </div>
+      <div className='link'>link</div>
+      
+     
+
+
+    </div>
   )
 }
 
--- ecommerpatik/src/components/Register.jsx
+++ ecommerpatik/src/components/Register.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+ <div className='log'>
+      <div className='recht'>
+        <img src="https://picsum.photos/200/300" alt="" />
+      </div>
+      <div className='left'>
+        sol
+      </div>
+
+
+    </div>
+
+    </div>
   )
 }
 
