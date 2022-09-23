0a4482ec2c7917c08fcb746cc010831ae2d048b7
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-09-23 19:34:04 +0200

    register

--- ecommerpatik/src/context/Auth.jsx
+++ ecommerpatik/src/context/Auth.jsx
@@ -1,115 +1,155 @@
 
 import axios from 'axios'
-import React,{createContext,useState,useEffect, useRef} from 'react'
+import React, { createContext, useState, useEffect, useRef } from 'react'
 import { nanoid } from 'nanoid'
 
 
 import { useNavigate } from 'react-router-dom'
 
 
- export const Authen = createContext()
+export const Authen = createContext()
 
-export  const AuthenProvider = (props)=>{
+export const AuthenProvider = (props) => {
 
-    const [load ,setLoad] =useState([])
+    const [load, setLoad] = useState([])
 
-    const [adana,setAdana] =useState(true)
+    const [adana, setAdana] = useState(false)
 
-    const [filter,setFilter] =useState()
+    const [filter, setFilter] = useState()
 
-    const [mail,setMail] =useState()
-    const [pass,setPass]= useState()
+    const [mail, setMail] = useState()
+    const [pass, setPass] = useState()
 
-    const [name,setName]=useState('')
-    const [email,setEmail] =useState('')
-    const [password,setPassword] = useState('')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    const [asa,setAsa] = useState([])
-
-  
-
- const yol =useNavigate()
- const selectRef = useRef()
- const minRef = useRef()
- const maxRef = useRef()
+    const [asal, setAsal] = useState({
+        id: '',
+        name: '',
+        email: '',
+        password: ''
+    })
 
 
 
- const handle1 =(e)=>{
-    try { 
-      
-        console.log(
-        minRef.current.value,
-        maxRef.current.value,
-        selectRef.current.value)
-     
-        
-    } catch (error) {
-        alert('navige')
+    const yol = useNavigate()
+    const selectRef = useRef()
+    const minRef = useRef()
+    const maxRef = useRef()
+
+
+
+    const handle1 = (e) => {
+        try {
+
+            console.log(
+                minRef.current.value,
+                maxRef.current.value,
+                selectRef.current.value)
+
+
+        } catch (error) {
+            alert('navige')
+        }
+
     }
-   
- }
 
- const handle2= (e)=>{
-    e.preventDefault()
-    console.log(mail,pass)
-    yol('/')
-    
- }
+    const handle2 = (e) => {
+        e.preventDefault()
+        console.log('de', mail, pass)
+        if(
+            asal.email == mail && asal.password == pass
+        )
+        {
+            setAdana(true)
+            yol('/')
+        }else{
+            alert('burdaysak bitik')
+        }
 
- const handle3 =(e)=>{
-    e.preventDefault()
-    console.log(name,email,password)
-   
 
-    asa.push({
-        id:nanoid(),
-        name:name,
-        email:email,
-        password:password
 
-      })
-      setAsa(asa)
-   
-console.log(asa)
 
- }
-    
 
-   
-    useEffect(()=>{
+    }
+
+    const handle3 = (e) => {
+        e.preventDefault()
+        console.log(name, email, password)
+
+        setAsal({
+            id: nanoid(),
+            name: name,
+            email: email,
+            password: password
+
+        })
+        console.log(asal)
+        yol('/login')
+
+       
+        /* 
+        burada dikkat edilmesi gerekli olan tek kullanici gibi olur
+        
+         const [asa,setAsa] = useState({
+             id:'',
+             name:'',
+             email:'',
+             password:''
+         })
+       */
+       
+        /*
+            asa.push({
+                id:nanoid(),
+                name:name,
+                email:email,
+                password:password
+        
+              })
+              setAsa(asa)
+              */
+
+
+    }
+
+
+
+    useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
-        .then((res)=>setLoad(res.data))
+            .then((res) => setLoad(res.data))
         console.log(load)
 
-    },[])
+    }, [])
 
 
 
 
-useEffect(()=>{
+    useEffect(() => {
 
-        const ada =load.filter((er)=>er.name.toLowerCase().includes(filter.toLowerCase()))
-        if(ada.length > 0 ){
+        const ada = load.filter((er) => er.name.toLowerCase().includes(filter.toLowerCase()))
+        if (ada.length > 0) {
             setLoad(ada)
         }
-      
-
-   
-
-},[filter])
 
 
 
 
-    return(
-<Authen.Provider value={{load ,filter,setFilter,adana,setAdana,selectRef,minRef,maxRef,handle1,mail,pass,setMail,setPass,handle2,email,setEmail,setPassword,password,handle3,name,setName}}>
-        {props.children}
-    </Authen.Provider>
+    }, [filter])
+
+
+
+
+
+    return (
+        <Authen.Provider value={{ load, filter, setFilter, adana, setAdana, selectRef, minRef, maxRef, handle1, mail, pass, setMail, setPass, handle2, email, setEmail, setPassword, password, handle3, name, setName }}>
+            {props.children}
+        </Authen.Provider>
     )
 
-    
- }
+
+}
 
 
 
