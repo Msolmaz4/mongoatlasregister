a776d654c8e5695f44995b0f8f52cd97e3f4735d
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-10-03 23:49:16 +0200

    allPro

--- postgresl/controller/todoController.js
+++ postgresl/controller/todoController.js
@@ -2,7 +2,21 @@ const {Sequelize}= require('sequelize')
 const TodModels = require('../models/TodModels')
 
 
-exports.addOneTodo =(req,res)=>{
-    console.log(req.body)
-    res.status(200).json({message:'sucses'})
+exports.addOneTodo = async(req,res)=>{
+    //console.log(req.body)
+     const {todo,urgent} = req.body
+     if(todo ){
+        await TodModels.create({todo:todo,urgent:urgent})
+        res.status(200).json({message:'sucses'})
+     }
+   
+}
+exports.getAllTodos=async(req,res)=>{
+    try {
+        
+        const allTodos =await TodModels.findAll()
+        res.status(200).send(allTodos)
+    } catch (error) {
+        console.log(error)
+    }
 }
--- postgresl/router/todoRouter.js
+++ postgresl/router/todoRouter.js
@@ -1,7 +1,9 @@
 const express =require('express')
 const router  = express.Router()
-const {addOneTodo} = require('../controller/todoController')
+const {addOneTodo,getAllTodos} = require('../controller/todoController')
 
+
+router.get('/',getAllTodos)
 router.post('/',addOneTodo)
 
 module.exports= router
