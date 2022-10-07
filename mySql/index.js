d2aec3965781b596bd5829a63222ff2b56d2e2a5
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-10-07 19:40:01 +0200

    cart

--- mySql/index.js
+++ mySql/index.js
@@ -1,3 +1,4 @@
+const { name } = require('ejs')
 const express = require('express')
 const app =express()
 const Port =8700
@@ -6,17 +7,26 @@ const Port =8700
 //burada ejs tanimla yoks ahata alirsin
 app.set('view engine','ejs')
 
+//public dosyasini acariy bu onmeli disari acmak gerekiyor
+//image kullandik direk yazdik
+app.use(express.static('public'))
+//ayrica node_module cikarmamiz gerekiyor
+//cunkyu botssrtap yukldeik onun icinsde nide module gidiyor
+app.use(express.static('node_modules'))
+
+
 const data = 
 [
-    { id:1 ,name:'iphone 12',price:'4000'},
-    { id:2 ,name:'iphone 1',price:'600'},
-    { id:3 ,name:'iphone',price:'9000'}
+    { id:1 ,name:'iphone 12',price:'4000',imageUrl:'1.jpg'},
+    { id:2 ,name:'iphone 1',price:'600',imageUrl:'2.jpg' },
+    { id:3 ,name:'iphone',price:'9000',imageUrl:'3.jpg'}
     
 ]
 
 app.use('/products/:id',(req,res)=>{
-    const urun =data.find(u=>u.id == req.params.id)
-    res.render('products-details',urun)
+    const deneme =data.find(u=>u.id == req.params.id)
+    res.render('productsdetails',deneme)
+    
 })
 
 //burada datayi gonderrirken direk gonderemeyiz
