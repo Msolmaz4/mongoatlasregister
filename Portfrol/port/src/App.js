e46975c548025763170a21c6793d74f5dc9c98fe
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-10-09 12:46:55 +0200

    mail

--- Portfrol/port/src/App.js
+++ Portfrol/port/src/App.js
@@ -1,6 +1,9 @@
 import Navbar from "./components/Navbar";
 import Home from './components/Home'
 import About from "./components/About";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
       <Navbar/>
       <Home/>
       <About/>
+      <Skills/>
+      <Work/>
+      <Contact/>
     </div>
   );
 }
